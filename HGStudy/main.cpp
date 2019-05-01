612eda6e5105ab1901fa7fc77dda6e121039251c
huangjianwu <kenmuhuang@xinli001.com>
2019-05-01 20:28:52 +0800

    修改下

---
+++ HGStudy/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HGStudy
+//
+//  Created by KenmuHuang on 2019-05-01.
+//  Copyright © 2019 KenmuHuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+
+}
