0944601f3784a2e139f19f1b0e851d15d234acd9
huangjianwu <kenmuhuang@xinli001.com>
2019-05-01 23:36:15 +0800

    [feat] 测试下效果

--- HGStudy/第一章 基本数据与表达式/BasicDataAndExpression.h
+++ HGStudy/第一章 基本数据与表达式/BasicDataAndExpression.h
@@ -11,7 +11,19 @@
 using namespace std;
 
 class BasicDataAndExpression {
-
+public:
+    void testMethod() {
+        double r = 0.0, girth, area;
+        const double PI = 3.1415;
+        cout << "Please input radius:\n";
+        cin >> r;
+        girth = 2 * PI * r;
+        area = PI * r * r;
+        cout << "radius = " << r << endl;
+        cout << "girth = " << girth << endl;
+        cout << "area = " << area << endl;
+        cout << "Hello KenmuHuang" << endl;
+    }
 };
 
 
